f4508026d68c6108da3f8e47b364cf736503e624
Вадим <vkarzakov31@gmail.com>
2024-05-19 17:22:18 +0300

    Nas

--- Program.cs
+++ Program.cs
@@ -10,15 +10,11 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
+    app.UseHttpsRedirection(); // Перенаправление HTTPS включено вне среды разработки
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
